202006a8b32d09b6cb7fc549d93876fb559b7aad
nageshwalchtwar <95535586+nageshwalchtwar@users.noreply.github.com>
2023-08-05 13:31:26 +0530

    Update lab_selenium.py

--- lab_selenium.py
+++ lab_selenium.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
+from webdriver_manager.chrome import ChromeType
 from selenium.webdriver.chrome.service import Service
 
 from selenium.webdriver.common.by import By
