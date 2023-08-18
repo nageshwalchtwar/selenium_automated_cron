f09c7840815f5f7a710e37185248d52567778e9b
nageshwalchtwar <95535586+nageshwalchtwar@users.noreply.github.com>
2023-08-18 17:10:27 +0530

    Update lab_selenium.py

--- lab_selenium.py
+++ lab_selenium.py
@@ -66,15 +66,17 @@ total = 0
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import urllib.request
+import undetected_chromedriver as uc
 
 try:
-        service = Service(ChromeDriverManager().install())
+    service = Service(ChromeDriverManager().install())
 except ValueError:
-        latest_chromedriver_version_url = "https://chromedriver.storage.googleapis.com/LATEST_RELEASE"
-        latest_chromedriver_version = urllib.request.urlopen(latest_chromedriver_version_url).read().decode('utf-8')
-        service = Service(ChromeDriverManager(version=latest_chromedriver_version).install())
-        driver_executable_path = service.path
-driver = webdriver.Chrome(options=chrome_options, driver_executable_path=driver_executable_path)
+    latest_chromedriver_version_url = "https://chromedriver.storage.googleapis.com/LATEST_RELEASE"
+    latest_chromedriver_version = urllib.request.urlopen(latest_chromedriver_version_url).read().decode('utf-8')
+    service = Service(ChromeDriverManager(version=latest_chromedriver_version).install())
+driver_executable_path = service.path
+
+options = uc.ChromeOptions()
 
 
 # Load the updated YAML file
