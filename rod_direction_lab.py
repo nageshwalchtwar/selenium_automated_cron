9fbcf57b7df7bc820cecb3e739bc90a0a6d69de9
nageshwalchtwar <95535586+nageshwalchtwar@users.noreply.github.com>
2023-08-25 19:47:44 +0530

    Update rod_direction_lab.py

--- rod_direction_lab.py
+++ rod_direction_lab.py
@@ -44,7 +44,7 @@ logging.basicConfig(filename='lab_direction.log', filemode='a',format='%(asctime
 
 # # Add the handler to the logger
 # logger.addHandler(handler)
-level= apparatus_level.liquid_levels
+# level= apparatus_level.get_liquid_levels
 load_dotenv()
 ss_paths = ss_4_lab.ss_paths
 print(ss_paths)
@@ -214,12 +214,12 @@ print(dir_log_ent)
 # beaker liquid
 # liquid_level_values = list(level.values())
 # print("Liquid level of oil and water is approx. : " ,liquid_level_values)
-lev=0
-for beaker, level in level.items():
-    if level < 90:
-        lev=1
-    else:
-        lev=0
+# lev=0
+# for beaker, level in level.items():
+#     if level < 90:
+#         lev=1
+#     else:
+#         lev=0
 
 
 l = ["moving_down_lab/","moving_up_lab/"]
@@ -237,7 +237,7 @@ for i in l:
     elif movement_code == "still":
         logging.warning('Movement: Still\n')
 
-if direction_check[0] == direction_check[1] == "still" or lev==1 :
+if direction_check[0] == direction_check[1] == "still" :
     recipients = ["nageshwalchtwar257@gmail.com"]
     send_email(recipients, '''Hi, I'm Vanishing Rod,
                                                 The experiment is having some issues, the Rods are still or the Video stream not showing during the process or liquid level is LOW. Kindly check the experiment 
