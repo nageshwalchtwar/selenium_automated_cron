b3905d6fa5f7a38d1a1aa09ee5208860b310abe3
nageshwalchtwar <95535586+nageshwalchtwar@users.noreply.github.com>
2023-09-07 11:14:42 +0530

    Update rod_direction_lab.py

--- rod_direction_lab.py
+++ rod_direction_lab.py
@@ -241,6 +241,8 @@ for i in l:
         logging.warning('Movement: Still\n')
 
 if direction_check[0] == direction_check[1] == "still" :
+    global status
+    status = "Not Working"
     recipients = ["nageshwalchtwar257@gmail.com", "vedant.nipane@students.iiit.ac.in","rishabh.agrawal@students.iiit.ac.in","abhinav.marri@research.iiit.ac.in"]
     send_email(recipients, '''Hi, I'm Vanishing Rod,
                                                 The experiment is having some issues, the Rods are still or the Video stream not showing during the process or liquid level is LOW. Kindly check the experiment 
@@ -260,6 +262,7 @@ elif direction_check[0] == "up" and direction_check[1] == "down":
                                                     - Maintainance Team ( Vanishing Rod )''', 'mail sent')
 elif direction_check[0] == "down" and direction_check[1] == "up" or direction_check[1]=="still" or direction_check[0]=="Distances list is empty":
     recipients = ["theccbussiness@gmail.com"]
+    status = "Working"
     logging.info('Works successfully\n')
     send_email(recipients, '''Hi,I'm Vanishing Rod, experiment working fine. The latency (seconds) is {match}!
                 - Maintainance Team ( Vanishing Rod ) '''.format(match=match) , 'mail sent')
@@ -270,6 +273,19 @@ elif direction_check[0] == "down" and direction_check[1] == "up" or direction_ch
 print(direction_check)
 
 
+import json
+
+data = {
+    "value": status
+}
+
+with open('data.json', 'w') as json_file:
+    json.dump(data, json_file)
+
+# Add, commit, and push the changes
+subprocess.run(["git", "add", "data.json"])
+subprocess.run(["git", "commit", "-m", "Update data.json"])
+subprocess.run(["git", "push", "origin", "main"]) 
 
 
 
